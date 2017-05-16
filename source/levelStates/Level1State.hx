01ad8960f207a32fc7511b802e8936fe8490787b
john <lij38@uw.edu>
2017-05-15 23:25:11 -0700

    instruction added

--- source/levelStates/Level1State.hx
+++ source/levelStates/Level1State.hx
@@ -15,6 +15,9 @@ import enemies.*;
 
 class Level1State extends PlayState
 {
+    private var _keys:FlxTilemap;
+    private var texts:FlxTypedGroup<FlxText>; 
+
     override public function create():Void 
     {
         ////////////////////////
@@ -23,6 +26,7 @@ class Level1State extends PlayState
          _map = new TiledMap(AssetPaths.lvl1__tmx);
         _background = new FlxTilemap();
         _plat = new FlxTilemap();
+        _keys = new FlxTilemap();
 
         //load background
         _background.loadMapFromArray(cast(_map.getLayer("background"), TiledTileLayer).tileArray, _map.width,
@@ -31,12 +35,46 @@ class Level1State extends PlayState
         _plat.loadMapFromArray(cast(_map.getLayer("plat"), TiledTileLayer).tileArray, _map.width,
             _map.height, AssetPaths.green__jpg, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 7209, 7208, 7209);
         
+		//load keys
+        _keys.loadMapFromArray(cast(_map.getLayer("keys"), TiledTileLayer).tileArray, _map.width,
+            _map.height, AssetPaths.keys__png, _map.tileWidth, _map.tileHeight, FlxTilemapAutoTiling.OFF, 7210, 7209, 8085);
+        
         _background.setTileProperties(1, FlxObject.NONE);
-        //_plat.setTileProperties(1, FlxObject.ANY);
+        _plat.setTileProperties(7209, FlxObject.ANY);
+		_keys.setTileProperties(7210, FlxObject.ANY);
         _background.follow();
 		_plat.follow();
+		_keys.follow();
+
+
+        /////////////////////////////////////////////
+        //LOAD INSTRUCTIONS
+		////////////////////////////////////////////
+		texts = new FlxTypedGroup<FlxText>();
+		var instructLayer:TiledObjectLayer = cast _map.getLayer("instruction");
+		for (e in instructLayer.objects) {
+			placeInstructions(e.name, e.xmlData.x);
+		}
 
         super.create();
         Main.LOGGER.logLevelStart(2);
+        add(_keys);
+		add(texts);
+    }
+
+
+    private function placeInstructions(entityName:String, entityData:Xml):Void 
+    {
+		var x:Int = Std.parseInt(entityData.get("x"));
+		var y:Int = Std.parseInt(entityData.get("y"));
+		switch entityName {
+           case "WW":  var ww:FlxText = new FlxText(x, y, 120, "DoubleJump", 19); 
+                       ww.setFormat(AssetPaths.FONT, ww.size); 
+                       texts.add(ww);
+
+           case "Q":   var q:FlxText = new FlxText(x, y, 250, "Switch Weapon Configuration", 19);
+		               q.setFormat(AssetPaths.FONT, q.size);
+                       texts.add(q);
+       }
     }
 }
