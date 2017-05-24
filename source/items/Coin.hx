54a16d0e427e1f9c4da01b93b632732223771e56
Pinyi Wang <pinyiwang123@gmail.com>
2017-05-24 03:23:10 -0700

    implemented potion

--- source/items/Coin.hx
+++ source/items/Coin.hx
@@ -1,20 +1,37 @@
 package items;
 
+import animation.Dialog;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
+import items.Coin.ItemType;
+
+enum ItemType {
+	COIN;
+	POTION;
+	OTHER;
+}
 
 class Coin extends FlxSprite {
 	private var value:Int;
+	public var type:ItemType;
 	public var onPickUp:Dynamic->Dynamic->Void;
 	
-	public function new(X:Float = 0, Y:Float = 0, lowerBound:Int = 1, 
+	public function new(X:Float = 0, Y:Float = 0, type:ItemType,
+						lowerBound:Int = 1, 
 						upperBound:Int = 10, gravity:Float = 1000) {
 		super(X, Y);
 		
 		onPickUp = pickUpCoin;
+		
+		this.type = type;
+		if (type == COIN) {
+			onPickUp = pickUpCoin;
+		} else if (type == POTION) {
+			onPickUp = pickUpPotion;
+		}
 		
 		value = Std.int(Math.random() * (upperBound - lowerBound) + lowerBound);
 		acceleration.y = gravity;
@@ -25,6 +42,13 @@ class Coin extends FlxSprite {
 		loadGraphic(AssetPaths.coins__png, true, 45, 48);
 		animation.add("spin", [0, 1, 2, 3, 4, 5, 6, 7], 9, true);
 		animation.play("spin");
+		scale.set(0.75, 0.75);
+	}
+	
+	public function loadPotionGrahpic():Void {
+		loadGraphic(AssetPaths.healthPotion__png, true, 50, 63);
+		animation.add("stop", [0, 1, 2, 3, 4, 5, 6], 9, true);
+		animation.play("stop");
 		scale.set(0.75, 0.75);
 	}
 	
@@ -46,6 +70,10 @@ class Coin extends FlxSprite {
 	// call back function for picking up coin
 	public function pickUpCoin(player:Player, coin:Coin):Void {
 		player.pickUpCoin(coin.getValue());
+	}
+	
+	public function pickUpPotion(player:Player, coin:Coin):Void {
+		player.health += 20;
 	}
 	
 }
