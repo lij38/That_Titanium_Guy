2e5ae1fc25ca2f3503422d3b8d43fc9bd2ac6af0
Pinyi Wang <pinyiwang123@gmail.com>
2017-06-03 15:40:11 -0700

    add player sprite

--- logging.sql
+++ logging.sql
@@ -36,13 +36,18 @@ select avg((end_ts - start_ts) / 1000) as duration from (select t1.qid, t1.dqid,
 --count of finished per level
 select count(*) from (select t1.qid, t1.dqid, t1.log_q_id as start_id, t2.log_q_id as end_id, t1.q_detail as start_details, t2.q_detail as end_details,
  t1.client_ts as start_ts, t2.client_ts as end_ts from player_quests_log as t1 left outer join player_quests_log as t2 on t1.dqid = t2.dqid
-  and t2.q_s_id = 0 where t1.qid=1 and t1.q_s_id = 1 and t1.cid=2) as t where end_details is not NULL;
+  and t2.q_s_id = 0 where t1.qid=1 and t1.q_s_id = 1 and t1.cid=3) as t where end_details is not NULL;
 
 --count of losing
 select count(*) from (select t1.qid, t1.dqid, t1.log_q_id as start_id, t2.log_q_id as end_id, t1.q_detail as start_details, t2.q_detail as end_details,
  t1.client_ts as start_ts, t2.client_ts as end_ts from player_quests_log as t1 left outer join player_quests_log as t2 on t1.dqid = t2.dqid
-  and t2.q_s_id = 0 where t1.qid=1 and t1.q_s_id = 1 and t1.cid=2) as t where end_details = "{\"won\":false}";
+  and t2.q_s_id = 0 where t1.qid=1 and t1.q_s_id = 1 and t1.cid=3) as t where end_details = "{\"won\":false}";
 --count of winning
 select count(*) from (select t1.qid, t1.dqid, t1.log_q_id as start_id, t2.log_q_id as end_id, t1.q_detail as start_details, t2.q_detail as end_details,
  t1.client_ts as start_ts, t2.client_ts as end_ts from player_quests_log as t1 left outer join player_quests_log as t2 on t1.dqid = t2.dqid
-  and t2.q_s_id = 0 where t1.qid=2 and t1.q_s_id = 1 and t1.cid=2) as t where end_details = "{\"won\":true}";
+  and t2.q_s_id = 0 where t1.qid=1 and t1.q_s_id = 1 and t1.cid=3) as t where end_details like '%\"won\":true%';
+
+--
+select count(distinct t.uid) from (select t1.uid as uid, t1.qid, t1.dqid, t1.log_q_id as start_id, t2.log_q_id as end_id, t1.q_detail as start_details, t2.q_detail as end_details,
+ t1.client_ts as start_ts, t2.client_ts as end_ts from player_quests_log as t1 left outer join player_quests_log as t2 on t1.dqid = t2.dqid
+  and t2.q_s_id = 0 where t1.qid=1 and t1.q_s_id = 1 and t1.cid=3) as t where end_details is not NULL;
